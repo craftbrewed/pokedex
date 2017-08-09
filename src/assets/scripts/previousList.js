3ae443597874b03dcc06bc578a836db20848a4fa
Dave <dave@craftbrewed.io>
2017-08-09 11:44:34 -0700

    pokelist cleanup?

--- src/assets/scripts/previousList.js
+++ src/assets/scripts/previousList.js
@@ -1,1 +1,30 @@
 "use strict";
+
+var previousList = function(){
+    this.list = [];
+};
+
+previousList.prototype.clear = function(){
+    this.list.forEach( item => {
+        item.classList.remove('n1', 'n2', 'n3');
+    });
+
+    this.list = [];
+};
+
+previousList.prototype.reset = function(current){
+    this.clear();
+
+    var previous = current.previousElementSibling,
+        n = 0;
+
+    while(previous && n++ < 3){
+        previous.classList.add("n"+n);
+        this.previousList.push(previous);
+        previous = previous.previousElementSibling;
+    }
+};
+
+previousList.prototype.shift = function(n){
+
+};
